369c2f6d3f4fc161c46c19df96d85847ffd9bf74
Lucas van Staden <sales@proxiblue.com.au>
2021-05-05 16:57:46 +0000

    adjust theme to themes

--- src/registration.php
+++ src/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'HyvaTheme_Magento2MagepowCategories', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'HyvaThemes_Magento2MagepowCategories', __DIR__);
